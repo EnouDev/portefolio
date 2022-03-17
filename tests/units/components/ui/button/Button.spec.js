8f9c0afb8ecdd1705d23c7c9a8a4d83fff716651
Loris Ciprin <loris@ciprin.fr>
2022-03-17 19:05:01 +0100

    update ci

--- tests/units/components/ui/button/Button.spec.js
+++ tests/units/components/ui/button/Button.spec.js
@@ -4,6 +4,6 @@ import Button from "@/components/ui/button/Button.vue";
 describe("Button.vue", () => {
   it("should render a button", function () {
     mount(Button);
-    cy.get("button").should("not.exist");
+    cy.get("button").should("exist");
   });
 });
